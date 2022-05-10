b4e0ff524ee819ba4c3a5f16150835ebb06ab179
Realomergal <omergal189@gmail.com>
2022-05-10 19:37:18 +0300

    Updates

---
+++ src/errors/bad-request-error.ts
@@ -0,0 +1,12 @@
+import { CustomError } from "./custom-error";
+
+export class BadReqeustError extends CustomError {
+  statusCode = 400;
+  constructor(public message: string) {
+    super(message);
+    Object.setPrototypeOf(this, BadReqeustError.prototype);
+  }
+  serializeErrors() {
+    return [{ message: this.message }];
+  }
+}
---
+++ src/middlewares/current-user.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken'
+
+interface UserPayLoad {
+    id: string,
+    email: string
+}
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        currentUser?: UserPayLoad;
+    }
+}
+
+
+export const currentUser = (req: Request, res: Response, next: NextFunction) => {
+
+    if (!req.session?.jwt) {
+        return next();
+    }
+
+    try {
+        const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!) as UserPayLoad;
+        req.currentUser = payload;
+    } catch (err) { }
+
+    next();
+}
